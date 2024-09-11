c215a598f7544494f8352da04ee113d4ee07945c
abinav369 <abinav369@proton.me>
2024-09-11 20:56:47 +0530

    heahaha

--- task-03/sub-task 3/Python/code.py
+++ task-03/sub-task 3/Python/code.py
@@ -1,6 +1,6 @@
 n = int(input('Enter a number: '))
 for i in range((n + 1) // 2):
-    print(" " * ((n // 2) - i) + "*" * (2 * i + 1))
+    print(" " * ((n // 2) - i) +"*" * (2 * i + 1))
 for i in range((n // 2) - 1, -1, -1):
-    print(" " * ((n // 2) - i) + "*" * (2 * i + 1))
+    print(" " * ((n // 2) - i) +"*" * (2 * i + 1))
 
